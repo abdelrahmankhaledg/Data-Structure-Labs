b5048131b5c5dbe7d74757a2e97f5a984a8442d3
abdelrahmankhaledg <58565775+abdelrahmankhaledg@users.noreply.github.com>
2020-02-20 13:18:52 +0200

    Update CalculatorClass.java

--- Lab1/CalculatorClass.java
+++ Lab1/CalculatorClass.java
@@ -1,1 +1,16 @@
+public class CalculatorClass implements ICalculator {
+public int add(int x,int y) {
+	return x+y;
+}
+public float divide(int x,int y) {
+	if(y==0) {
+		throw new RuntimeException("You cannot divide by zero");
+	}
+	else if (x==0) {//this if statement because (float)0/-10 returns -0 and not 0 
+		return 0;
+	}
+	else
+	return (float)x/y;
+}
+}
 
