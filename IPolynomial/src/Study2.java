348703dc5e199b559bb346e278d40bc317769d3e
abdelrahmankhaledg <58565775+abdelrahmankhaledg@users.noreply.github.com>
2020-03-20 22:32:18 +0200

    Update Study2.java

--- IPolynomial/src/Study2.java
+++ IPolynomial/src/Study2.java
@@ -1,263 +1,233 @@
 
 
-import java.awt.dnd.DnDConstants;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class Study2 implements ILinkedList {
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
-    class DNode{
-        Object data;
-        DNode next;
-        DNode prev;
-    }
-    DNode head,tail;
-    static Study2 list2 = new Study2();
+class TStudy2 {
+static Study2 List=new Study2();
+static PolyNode P=new PolyNode(5,4);
+static PolyNode Q=new PolyNode(3,2);
+static PolyNode N=new PolyNode(3,3);
+static PolyNode G=new PolyNode(1,1);
+static PolyNode K=new PolyNode(7,0);
+@BeforeAll
+static void  BF() {
+	List.add(P);
+	List.add(Q);
+	List.add(N);
+	List.add(G);
+	List.add(K);
+}
+@Test
+void testAdd1() {
+	assertEquals(P,(List.get(0)));
+}
+@Test void testAdd2() {
+	assertEquals(Q,List.get(1));
+}
+@Test
+void testAdd3() {
+	assertEquals(N,List.get(2));
+}
+@Test
+void testAdd4() {
+	assertEquals(G,List.get(3));
+}
+@Test
+void testAdd5() {
+	assertEquals(K,List.get(4));
+}
+@Test 
+void testAdd6(){
+	PolyNode In=new PolyNode(7,8);
+	List.add(3, In);
+	PolyNode Out=(PolyNode)List.get(3);
+	assertEquals(7,Out.getCoeff());
+	assertEquals(8,Out.getExponent());
+}
+@Test
 
-    public static void main(String[] args){
-        list2.add(5);
-        list2.add(12);
-        list2.insertAtStart(10);
-        list2.insertAtStart(16);
-        list2.add(1,1);
+void testAdd7() {
+	PolyNode In=new PolyNode(1,2);
+	PolyNode Out=new PolyNode();
+	List.add(0,In);
+	Out=(PolyNode)List.get(0);
+	assertEquals(1,Out.getCoeff());
+	assertEquals(2,Out.getExponent());
+	
+}
+@Test 
+void testGet1() {
+	assertThrows(IllegalArgumentException.class,()-> List.get(7));
+}
+@Test 
+void testGet2() {
+	assertThrows(IllegalArgumentException.class,()-> List.get(-5));
+}
+@Test
+void testGet3() {
+	Study2 emptyList=new Study2();
+	assertThrows(IllegalArgumentException.class,()-> emptyList.get(3));
+}
 
-        list2.clear();
-        try{
-        list2.deleteAtStart();
-        }
-        catch (Exception e){
-            System.out.println("you can't Delete an empty list");
-        };
+@Test
+void testSet1() {
+	PolyNode In=new PolyNode(20,30);
+	List.set(2, In);
+	PolyNode Out=new PolyNode();
+	Out=(PolyNode)List.get(2);
+	assertEquals(20,Out.getCoeff());
+	assertEquals(30,Out.getExponent());
+	
+}
+@Test
+void testSet2() {
+	Study2 emptyList=new Study2();
+	assertThrows(RuntimeException.class,()-> emptyList.set(10, N));
+}
+@Test
+void testSet3() {
+	assertThrows(IllegalArgumentException.class,()->List.set(10, Q));
+}
+@Test
+void testSet4() {
+	assertThrows(IllegalArgumentException.class,()->List.set(-3, Q));
+}
+@Test
+void testSet5() {
+	PolyNode In=new PolyNode(4,5);
+	List.set(0, In);
+	PolyNode Out=(PolyNode)(List.get(0));
+	assertEquals(4,Out.getCoeff());
+	assertEquals(5,Out.getExponent());
+}
+@Test
+void testSet6() {
+	PolyNode In=new PolyNode(3,2);
+	List.set(List.size()-1, In);
+	PolyNode Out=new PolyNode();
+	Out=(PolyNode)(List.get(List.size()-1));
+	assertEquals(3,Out.getCoeff());
+	assertEquals(2,Out.getExponent());
+}
+
+@Test
+void testRemove1() {
+	List.remove(3);//We added (7,8) at 3  and added (1,2) at 1 so when removing it , PolyNode (7,8) will be 3 and Node N will be Removed
+	PolyNode Out=new PolyNode();
+	Out=(PolyNode)List.get(3);
+	assertEquals(7,Out.getCoeff());
+	assertEquals(8,Out.getExponent());
+	assertEquals(6,List.size());
+}
+@Test
+void testRemove2() {
+	Study2 emptyList=new Study2();
+	assertThrows(NullPointerException.class,()->emptyList.remove(5));
+}
+@Test
+void testRemove3() {
+	assertThrows(IllegalArgumentException.class,()->List.remove(20));
+}
+@Test 
+void testRemove4() {
+	List.remove(0);//We added (1,2) as the first element ,after removing it P will be the first
+	assertEquals(P,List.get(0));
+}
+@Test
+void testRemove5() {
+	List.remove(List.size()-1);//We removed the last node which was K but set to(3,2) ,after remove G will be the last node
+	assertEquals(G,List.get(List.size()-1));
+	
+}
+@Test
+void testContains1() {
+	assertTrue(List.contains(G));
+}
+@Test
+void testContains2() {
+	PolyNode H=new PolyNode(100,100);
+	assertFalse(List.contains(H));
+}
+@Test
+void testContains3() {
+	assertTrue(List.contains(P));
+}
+@Test
+void TheLastTest1() {
+	ILinkedList SubList=new Study2();
+	SubList=(Study2)(List.sublist(1,3));
+	assertEquals(3,SubList.size());
+	assertEquals(List.get(1),SubList.get(0));
+	assertEquals(List.get(2),SubList.get(1));
+	assertEquals(List.get(3),SubList.get(2));
+}
+@Test
+void TheLastTest2() {
+	assertThrows(IllegalArgumentException.class,()->List.sublist(-5, 10));
+}
+@Test
+void TheLastTest3() {
+	assertThrows(IllegalArgumentException.class,()->List.sublist(5, -10));
+}
+@Test
+void TheLastTest4() {
+	assertThrows(IllegalArgumentException.class,()->List.sublist(2, 1));
+}
+@Test
+void TheLastTest5() {
+	ILinkedList SubList=new Study2();
+	SubList=(Study2)(List.sublist(0,2));
+	assertEquals(3,SubList.size());
+	assertEquals(List.get(0),SubList.get(0));
+	assertEquals(List.get(1),SubList.get(1));
+	assertEquals(List.get(2),SubList.get(2));
+}
+@Test
+void TheLastTest6() {
+	assertThrows(IllegalArgumentException.class,()->List.sublist(3, 6));
+}
+@Test
+void TheLastTest7() {
+	assertThrows(IllegalArgumentException.class,()->List.sublist(3, 6));
+}
+@Test 
+void TheLastTest8() {
+	PolyNode C=new PolyNode(12,15);
+	assertThrows(IllegalArgumentException.class,()->List.add(-5, C));
+}
+@Test
+void TheLastTest9() {
+	PolyNode C=new PolyNode(12,15);
+	assertThrows(IllegalArgumentException.class,()->List.add(5, C));
+}
+@Test
+void TheLastTestK() {
+	assertThrows(IllegalArgumentException.class,()->List.add(12,P));;
+}
+@Test
+void TheLastTestJ() {
+	assertThrows(IllegalArgumentException.class,()->List.remove(-5));
+}
+
+@Test
+void TheLastTestm() {
+	PolyNode In =new PolyNode(20,21);
+	List.add(4,In);
+	PolyNode Out=new PolyNode();
+	Out=(PolyNode)(List.get(4));
+	assertEquals(In,Out);
+}
+
+@Test 
+void TheLastTestn() {
+	List.clear();
+	assertEquals(0,List.size());
+	assertTrue(List.isEmpty());
+}
 
 
-        //System.out.println(list2.size());
-        //list2.show();
-    }
 
-
-
-    public void insertAtStart(Object element){ // insert element at the start
-        DNode node = new DNode();
-        node.data=element;
-        node.next=head;
-        node.prev=null;
-        head=node;
-    }
-
-    public void deleteAtStart() throws NullPointerException{
-        if(head == null){
-            throw new NullPointerException("You can't delete an empty List");
-        }else if (head.next==null){
-            head=null;
-        }else{
-            head=head.next;
-            head.prev=null;
-        }
-
-    }
-
-    @Override
-    public void add(int index, Object element) {
-        DNode node = new DNode();
-        node.data=element;
-        if(index==0){
-            insertAtStart(element);
-        }
-        else if(index<0) {
-        	throw new IllegalArgumentException();
-        }
-        else{
-            DNode n = head;
-            for(int i=0; i<index-1;i++){
-            	if(n!=null)
-                n= n.next;
-            }
-            if(n==null)
-            	throw new IllegalArgumentException();
-            if(n.next!=null) {
-            n.next.prev=node;
-            }
-            node.next=n.next;
-            node.prev=n;
-            n.next=node;
-        }
-
-
-    }
-
-    @Override
-    public void add(Object element) {
-        DNode node = new DNode();
-        node.data=element;
-        if(head == null){
-            head=tail=node;
-            head.prev=null;
-            tail.next=null;
-        }else{
-            tail.next=node;
-            node.prev=tail;
-            tail=node;
-            tail.next=null;
-        }
-    }
-
-    public void show(){
-        DNode node = head;
-        if(head == null){
-            throw new NullPointerException();
-        }else{
-            while (node!=null){
-                System.out.println(node.data);
-                node=node.next;
-            }
-        }
-    }
-
-    @Override
-    public Object get(int index) {
-        DNode node = head;
-        if(node ==null) {
-        	throw new IllegalArgumentException();
-        }
-        if(index<0) {
-        	throw new IllegalArgumentException();
-        }
-        for(int i =0 ;i<index;i++){
-        	if(node.next!=null)
-            node = node.next;
-        	else 
-        	throw new IllegalArgumentException();
-        }
-        return node.data;
-    }
-
-    @Override
-    public void set(int index, Object element) {
-        DNode node = head;
-        if(node ==null) {
-        	throw new RuntimeException();
-        }
-        else if (index<0) {
-        	throw new IllegalArgumentException();
-        }
-        for(int i=0 ;i <index;i++){
-        	if(node.next!=null)
-            node=node.next;
-        	else 
-        		throw new IllegalArgumentException();
-        }
-        node.data=element;
-    }
-
-    @Override
-    public void clear() {
-        if (head == null) {
-            throw new NullPointerException();
-        } else {
-            DNode node = head;
-            while (node != null) {
-                node = node.next;
-                deleteAtStart();
-            }
-        }
-    }
-
-    @Override
-    public boolean isEmpty() {
-        if(head == null){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-    @Override
-    public void remove(int index) {
-        if (head == null) {
-            throw new NullPointerException();
-        }
-        else if(index<0) 
-        	throw new IllegalArgumentException();
-        	else {
-            DNode node = head;
-            DNode m=null;
-            if (index == 0) {
-                deleteAtStart();
-            } else {
-                for (int i = 0; i < index - 1; i++) {
-                	if(node.next!=null)
-                    node = node.next;
-                	else 
-                		throw new IllegalArgumentException();//List node of index < index-1 or equal to it doesn't exist 
-                }
-               m=node.next;
-                if(m==null)
-                	throw new IllegalArgumentException();//The node of index==index doesn't Exist
-               
-                node.next = m.next;
-                if(node.next!=null)
-                node.next.prev = node;
-     
-            }
-        }
-    }
-
-    @Override
-    public int size() {
-        DNode n = head;
-        int count=0;
-        if(head == null){
-            return count;
-        }else{
-            while(n!=null){
-                count++;
-                n=n.next;
-            }
-            return count;
-        }
-    }
-
-    @Override
-    public ILinkedList sublist(int fromIndex, int toIndex) {
-        Study2 kilo = new Study2();
-        DNode n=head;
-        if(n==null) {
-        	throw new IllegalArgumentException();
-        }
-        else if(fromIndex<0||toIndex<0||toIndex<fromIndex) {
-        	throw new IllegalArgumentException();
-        }
-        for(int j=0;j <fromIndex;j++) {
-        	if(n.next!=null)  
-        		n=n.next;
-        		else 
-        		throw new IllegalArgumentException();
-        }
-        for(int i = fromIndex; i<=toIndex; i++){
-            if(n!=null) {
-            	kilo.add(n.data);
-            	n=n.next;
-            }
-            else 
-            	throw new IllegalArgumentException();
- 
-        }
-        //kilo.show();
-        return kilo;
-    }
-
-    @Override
-    public boolean contains(Object o) {
-        DNode n=head;
-        while(n.next!=null){
-            if(n.data==o){
-                return true;
-            }else{
-                n=n.next;
-            }
-        }
-        if(n.data==o){
-            return true;
-        }else{
-            return false;
-        }
-    }
 }
